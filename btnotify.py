7beb06837e11379c3d1e260f1b184faa431e96cc
Xarvis-PC <shivanshu26shiv@gmail.com>
2018-11-09 23:38:43 +0530

    NA

--- btnotify.py
+++ btnotify.py
@@ -1,6 +1,12 @@
 '''
 This code will notify user is battery goes beyond certain level
+By: Shivanshu
+Purpose: Currently my notebook shuts down abnormally after 50% battery,
+Hence I have created a script that will notify me as soon as battery charge reaches 55%
+Later I converted this file to exe using "pyinstaller -w scriptname.py command",
+then pushed the executable into Windows startup.
 '''
+
 
 import time
 import psutil
@@ -15,7 +21,6 @@ while(1):
         os.system("start cmd")
         winsound.Beep(200,1000)
     '''
-    
         notification.notify(
             title='Battery status!',
             message='Current battery: '+str(battery[0])+'%',
