7a8a0ebc81c522fe7545e3aca93036d1e72f417c
Shivanshu26shiv <Shivanshu26shiv@gmail.com>
2019-04-23 00:36:15 +0530

    Rename dulpicate_char_rem.py to dulplicate_char_rem.py

--- dulpicate_char_rem.py
+++
@@ -1,7 +0,0 @@
-'''
-Purpose: To remove duplicates from a string in a pythonic way
-By: Shivanshu
-'''
-
-l=[]
-print ''.join(l) if map(lambda x: l.append(x) if x not in l else None,'Mahatama Gandhi') else 0
