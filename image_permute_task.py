ae52fdf970cfb1509e5a59bfee6bf81e897469d6
Shivanshu Srivastava <Shivanshu26shiv@gmail.com>
2020-04-19 18:35:03 +0530

    Update image_permute_task.py

--- image_permute_task.py
+++ image_permute_task.py
@@ -1,3 +1,20 @@
+'''
+We need a Python program that generates all the possible flags with 3 colored stripes, see attachment.
+The colors can be: White, Black, Green, Red, Blue, Green and Orange.
+
+Question 1: Please generate alle the possible flags. One color van not be the same in two stripes next to each other. So green, white, green is possible.
+
+Question 2: Please generate alle the possible flags. One color can only be used onces in a flag. So green, white, green is not good, but So green, white, blue is possible.
+
+Make a Python-program (for mathematic education) that makes this visable. We leave it to your creativity if it is possible to show the flags very small on the screen.
+
+It would be very nice if you can help us!
+
+Thanks in advance, Durk Jan de Bruin
+
+PS: Not all the flags exist in real countries. That is no problem
+'''
+
 from PIL import Image, ImageColor
 from itertools import permutations, product
 
